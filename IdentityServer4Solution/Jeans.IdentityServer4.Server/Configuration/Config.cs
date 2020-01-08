3b581263a8b3917bfd11d4d8551d79d8cefce682
Jian Li <lijian15@cn.wilmar-intl.com>
2020-01-08 10:09:10 +0800

    1

---
+++ IdentityServer4Solution/Jeans.IdentityServer4.Server/Configuration/Config.cs
@@ -0,0 +1,40 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jeans.IdentityServer4.Server.Configuration
+{
+    public class Config
+    {
+        public static List<Client> GetClients()
+        {
+            var clientList = new List<Client>();
+            clientList.Add(new Client
+            {
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                ClientId = "Jeans",
+                ClientSecrets = new List<Secret>
+                {
+                    new Secret("123456".Sha256())
+                }
+            });
+
+            return clientList;
+        }
+
+        public static List<ApiResource> GetApiResources()
+        {
+            var apiList = new List<ApiResource>();
+            apiList.Add(new ApiResource
+            {
+                Name="user",
+                Description="获取用户信息"
+            });
+
+            return apiList;
+        }
+
+    }
+}
---
+++ IdentityServer4Solution/Jeans.IdentityServer4.Server/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Jeans.IdentityServer4.Server.Models;
+
+namespace Jeans.IdentityServer4.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
