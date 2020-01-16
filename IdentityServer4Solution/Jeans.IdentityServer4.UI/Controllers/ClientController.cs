1f36656202c16f2bf13bda10e75b3d561785f516
Jian Li <lijian15@cn.wilmar-intl.com>
2020-01-16 14:26:47 +0800

    delete datatables

---
+++ IdentityServer4Solution/Jeans.IdentityServer4.UI/Controllers/ClientController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jeans.IdentityServer4.UI.Controllers
+{
+    /// <summary>
+    /// 客户端
+    /// </summary>
+    public class ClientController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
