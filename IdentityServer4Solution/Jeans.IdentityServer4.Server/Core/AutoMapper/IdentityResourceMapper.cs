594794295d31818622e131da4883b41d1dbbe7eb
Jian Li <lijian15@cn.wilmar-intl.com>
2020-03-24 17:26:12 +0800

    资源 mapper

---
+++ IdentityServer4Solution/Jeans.IdentityServer4.Server/Core/AutoMapper/IdentityResourceMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jeans.IdentityServer4.Server.Core.AutoMapper
+{
+    public static class IdentityResourceMapper
+    {
+
+    }
+}
